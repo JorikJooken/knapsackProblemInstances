1b63bda0c05f4dbe18aec665505e93fd9dbb4e1c
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2024-02-02 15:50:46 -0300

    refactor: replace 'define' directive by 'using'

--- generator.cpp
+++ generator.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <random>
+#include <cstdint>
+
+using Integer = std::int64_t;
 
 int main()
 {
-    #define int long long
-
     std::cerr << "n=" << std::endl;
-    int n;
+    Integer n;
     std::cin >> n;
     std::cerr << "capacity=" << std::endl;
     long long cap;
     std::cin >> cap;
     std::cerr << "classes=" << std::endl; // >= 2
-    int classes;
+    Integer classes;
     std::cin >> classes;
     classes--;
     std::cerr << "fraction=" << std::endl; // 0<=frac<=1, in practice frac should be quite a lot smaller than 1
@@ -26,27 +27,27 @@ int main()
     std::cin >> small;
 
     std::mt19937 generator(1234);
-    std::uniform_int_distribution<int> distribution(1,small);
+    std::uniform_int_distribution<Integer> distribution(1,small);
     std::cout << n << std::endl;
-    int amountSmall=n*frac;
-    int am1=(n-amountSmall)/classes;
+    Integer amountSmall=n*frac;
+    Integer am1=(n-amountSmall)/classes;
     double denominator=2.0;
-    int amountCtr=0;
-    for(int j=0; j<classes; j++)
+    Integer amountCtr=0;
+    for(Integer j=0; j<classes; j++)
     {
-        for(int i=0; i<am1; i++)
+        for(Integer i=0; i<am1; i++)
         {
-            int num1=distribution(generator);
-            int num2=distribution(generator);
-            std::cout << amountCtr << " " << (int)((1/denominator+eps)*cap+num1) << " " << (int)((1/denominator+eps)*cap+num2) << std::endl;
+            Integer num1=distribution(generator);
+            Integer num2=distribution(generator);
+            std::cout << amountCtr << " " << (Integer)((1/denominator+eps)*cap+num1) << " " << (Integer)((1/denominator+eps)*cap+num2) << std::endl;
             amountCtr++;
         }
         denominator*=2;
     }
-    for(int i=amountCtr; i<n; i++)
+    for(Integer i=amountCtr; i<n; i++)
     {
-        int num1=distribution(generator);
-        int num2=distribution(generator);
+        Integer num1=distribution(generator);
+        Integer num2=distribution(generator);
         std::cout << i << " " << num1 << " " << num2 << std::endl;
     }
     std::cout << cap << std::endl;
